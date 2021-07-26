163d9ab0fb244b9da27cf29142f0ba06ca01d585
LeeHsss <lii0730@icloud.com>
2021-07-26 21:02:43 +0900

    Initial Commit

---
+++ WebViewTest/WebViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewTestApp.swift
+//  WebViewTest
+//
+//  Created by LeeHsss on 2021/07/26.
+//
+
+import SwiftUI
+
+@main
+struct WebViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
